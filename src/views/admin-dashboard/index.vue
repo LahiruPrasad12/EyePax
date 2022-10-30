3c03681246c1b7739388153167f7d36eee91732d
LahiruPrasad12 <lahirupr471@gmail.com>
2022-10-30 22:08:24 +0530

    complted logout function

--- src/views/admin-dashboard/index.vue
+++ src/views/admin-dashboard/index.vue
@@ -1,62 +1,108 @@
 <template>
   <ion-page>
-
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
-      <ion-tab-bar style="padding-bottom:20px; background-color: black" slot="bottom">
-
+      <ion-tab-bar
+        style="padding-bottom: 20px; background-color: black"
+        slot="bottom"
+      >
         <ion-tab-button
-            href="/admin_home/dash_board"
-            selected
-            tab="dash_board"
-            v-on:click="afterTabChange('dash_board')" style="background-color: black">
-          <ion-icon :icon="homeOutline"/>
-          <ion-label v-show="tabName==='dash_board'" style="color: #5bf5a8;">Dashboard</ion-label>
-          <ion-label v-show="tabName !=='dash_board'" style="color: #ffffff;">Dashboard</ion-label>
+          href="/admin_home/dash_board"
+          selected
+          tab="dash_board"
+          v-on:click="afterTabChange('dash_board')"
+          style="background-color: black"
+        >
+          <ion-icon :icon="homeOutline" />
+          <ion-label v-show="tabName === 'dash_board'" style="color: #5bf5a8"
+            >Dashboard</ion-label
+          >
+          <ion-label v-show="tabName !== 'dash_board'" style="color: #ffffff"
+            >Dashboard</ion-label
+          >
         </ion-tab-button>
 
         <ion-tab-button
-            href="/admin_home/staff"
-            tab="staff"
-            v-on:click="afterTabChange('staff')" style="background-color: black">
-          <ion-icon :icon="peopleOutline"/>
-          <ion-label v-show="tabName==='staff'" style="color: #5bf5a8;">Staff</ion-label>
-          <ion-label v-show="tabName !=='staff'" style="color: #ffffff;">Staff</ion-label>
+          href="/admin_home/staff"
+          tab="staff"
+          v-on:click="afterTabChange('staff')"
+          style="background-color: black"
+        >
+          <ion-icon :icon="peopleOutline" />
+          <ion-label v-show="tabName === 'staff'" style="color: #5bf5a8"
+            >Staff</ion-label
+          >
+          <ion-label v-show="tabName !== 'staff'" style="color: #ffffff"
+            >Staff</ion-label
+          >
         </ion-tab-button>
 
         <ion-tab-button
-            href="/admin_home/tracking"
-            tab="tracking"
-            v-on:click="afterTabChange('tracking')" style="background-color: black">
-          <ion-icon :icon="mapOutline"/>
-          <ion-label v-show="tabName==='tracking'" style="color: #5bf5a8;">Tracking</ion-label>
-          <ion-label v-show="tabName !=='tracking'" style="color: #ffffff;">Tracking</ion-label>
-
+          href="/admin_home/tracking"
+          tab="tracking"
+          v-on:click="afterTabChange('tracking')"
+          style="background-color: black"
+        >
+          <ion-icon :icon="mapOutline" />
+          <ion-label v-show="tabName === 'tracking'" style="color: #5bf5a8"
+            >Tracking</ion-label
+          >
+          <ion-label v-show="tabName !== 'tracking'" style="color: #ffffff"
+            >Tracking</ion-label
+          >
         </ion-tab-button>
-        
-<!--        <ion-tab-button-->
-<!--            href="/tabs/leaves"-->
-<!--            tab="leaves"-->
-<!--            v-on:click="afterTabChange('leaves')">-->
-<!--          <ion-icon :icon="cafeOutline"/>-->
-<!--          <ion-label v-show="tabName==='leaves'" style="color: #000000;">Leaves</ion-label>-->
-<!--          <ion-label v-show="tabName !=='leaves'" style="color: #ffffff;">Leaves</ion-label>-->
-<!--        </ion-tab-button>-->
+
+        <ion-tab-button
+          @click="logout()"
+          tab="profile"
+          v-on:click="afterTabChange('profile')"
+          style="background-color: black"
+        >
+          <ion-icon :icon="personCircleOutline" />
+          <ion-label v-show="tabName === 'logout'" style="color: #5bf5a8"
+            >Logout</ion-label
+          >
+          <ion-label v-show="tabName !== 'logout'" style="color: #ffffff"
+            >Logout</ion-label
+          >
+        </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
-    <!--      <ion-tab-bar slot="bottom">-->
-    <!--      </ion-tab-bar>-->
   </ion-page>
 </template>
 
 <script>
-import {IonIcon, IonLabel, IonPage, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs} from '@ionic/vue';
-import {cafeOutline, calendarOutline, homeOutline, timeOutline,peopleOutline, mapOutline} from 'ionicons/icons';
-import {useRouter} from "vue-router";
-import {defineComponent} from "vue";
+import {
+  IonIcon,
+  IonLabel,
+  IonPage,
+  IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+} from "@ionic/vue";
+import {
+  cafeOutline,
+  calendarOutline,
+  homeOutline,
+  timeOutline,
+  peopleOutline,
+  mapOutline,
+  personCircleOutline,
+} from "ionicons/icons";
+import { useRouter } from "vue-router";
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  components: {IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage, IonRouterOutlet},
+  components: {
+    IonLabel,
+    IonTabs,
+    IonTabBar,
+    IonTabButton,
+    IonIcon,
+    IonPage,
+    IonRouterOutlet,
+  },
   methods: {
     async beforeTabChange() {
       this.no -= 1;
@@ -68,9 +114,12 @@ export default defineComponent({
     async afterTabChange(tabName) {
       this.tabName = tabName;
     },
+    logout(){
+      localStorage.removeItem('token')
+      this.router.push('/login')
+    }
   },
   setup() {
-
     const router = useRouter();
     return {
       no: 1,
@@ -80,8 +129,9 @@ export default defineComponent({
       mapOutline,
       peopleOutline,
       timeOutline,
+      personCircleOutline,
       router,
-    }
+    };
   },
   data() {
     return {
@@ -92,6 +142,6 @@ export default defineComponent({
       tab3: "",
       tab4: "",
     };
-  }
+  },
 });
 </script>
